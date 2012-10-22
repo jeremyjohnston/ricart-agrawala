259054ed7c497393382d0479051e57f791b228f9
David A. Sanders, Jr. <das063000@utdallas.edu>
2012-10-22 17:12:16 -0500

    Here We Go
    Modifications from last time largely recreated

--- Driver.java
+++ Driver.java
@@ -3,78 +3,82 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class Driver {
-	
-	public ArrayList<Socket> sockets = new ArrayList<Socket>();
-	
-	public ArrayList<PrintWriter> writers = new ArrayList<PrintWriter>();
-	
-	public ArrayList<InputStreamReader> streamReaders = new ArrayList<InputStreamReader>();
-	public ArrayList<BufferedReader> readers = new ArrayList<BufferedReader>();
-	
-	//TODO: Maybe find some means of not using predetermined addresses
-	// A fixed number of ip addresses
-	public ArrayList<String> ipAddresses = new ArrayList<String>();
-	
-	// and ports
-	public ArrayList<Integer> ports = new ArrayList<Integer>();
-	
-	// Just some arbitrary fixed count for now
-	public int totalNodeCount = 5;
+public class Driver 
+{
 
 	/** Start the driver, with a number of channels specified. **/
-	public Driver(int nodeAssignment){
+	public Driver(String[] args)
+	{
+		final boolean desireToHarmHumansOrThroughInactionAllowHumansToComeToHarm = false;
+		//Just in case
+		String nodeName = "P"; //Default
+		int nodeNum = 1;
+		int portNumber = 3009; //Default
+		String network2 = "net02.utdallas.edu"; //Default network for one of the other nodes
+		String network3 = "net03.utdallas.edu"; //Default network for one of the other nodes
 		
-		RicartAgrawala me = new RicartAgrawala(nodeAssignment, 0);
-		
-		//create N channels, 1 to each other process
-		//TODO: Rather than assuming connections and passing number of processes, have processes register with the 0th process
-		for(int i = 0; i < totalNodeCount; i++){
-			
-			if(i == nodeAssignment)
-				continue;
-			
-			
-			try{
-				Socket s = new Socket(ipAddresses.get(i), ports.get(i));
-				sockets.add(s);
-				
-				PrintWriter w = new PrintWriter(s.getOutputStream());
-				writers.add(w);
-				
-				//TODO: similarly for readers
-				
-			}catch(Exception ex){
-				ex.printStackTrace();
+		if(args.length != 0)
+		{
+			nodeName = args[0];
+			nodeNum = Integer.parseInt(args[1]);
+			portNumber = Integer.parseInt(args[2]);
+			if (args.length > 2)
+			{
+				network2 = args[3];
+				network3 = args[4];
 			}
-			
 		}
 		
+		int numberOfWrites = 0;
+		
+		RicartAgrawala me = new RicartAgrawala(nodeNum, 0);
 		
 		
+		//TODO: Rather than assuming connections and passing number of processes, have processes register with the 0th process
+
+		try
+		{
+			Socket s1 = new Socket();
+			Socket s2 = new Socket();
+			PrintWriter w1 = new PrintWriter(s1.getOutputStream());
+			PrintWriter w2 = new PrintWriter(s2.getOutputStream());
+			//TODO: similarly for readers	
+		}
+		catch(Exception ex){ ex.printStackTrace();}
+			
 		//Launch thread that occasionally calls requestCS(me) and attempts CS
 		//Also thread to read channels
 		
 	}
 	
 	/** Invocation of Critical Section*/
-	public void requestCS(RicartAgrawala me){
+	public static boolean criticalSection(String nodeName, int numberOfWrites)
+	{
+		try
+		{
+			BufferedWriter criticalSection = new BufferedWriter(new FileWriter("CriticalSectionOutput"));
+			criticalSection.write(nodeName + " has now accessed it's critical section " + numberOfWrites + " times.");
+			criticalSection.flush(); //flush stream
+			criticalSection.close(); //close write
+		} 
+		catch(IOException e){ System.out.println("Oh No! Something Has Gone Horribly Wrong");}
+		return true;
+	}
+	
+	public void requestCS(RicartAgrawala me)
+	{
 		me.invocation();
-		
-		for(PrintWriter w: writers){
-			w.println("REQUEST,"+ me.nodeNum);
-		}
+		//w1.println("REQUEST,"+ me.nodeNum);
 	}
 	
-	public void releaseCS(RicartAgrawala me){
-		for(PrintWriter w: writers){
-			w.println("RELEASE,"+ me.nodeNum);
-		}
+	public void releaseCS(RicartAgrawala me)
+	{
+		//w1.println("RELEASE,"+ me.nodeNum);
 	}
 	
-	public static void main(String[] args) {
-		new Driver(Integer.parseInt(args[0]));
-		
+	public static void main(String[] args) 
+	{
+		new Driver(args);	
 	}
 
 }
